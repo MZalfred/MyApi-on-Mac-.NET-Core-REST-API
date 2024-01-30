1ed0e205697adc5777280701a3bada300b6eec18
MZalfred <cutesy-todo0x@icloud.com>
2024-01-30 15:24:55 +0000

    main

--- MyApiProject/obj/Debug/net8.0/MyApiProject.AssemblyInfo.cs
+++ MyApiProject/obj/Debug/net8.0/MyApiProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyApiProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+848b8ac6d5cae8e12f235001894778a1fa1ebe4a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f47a8fe1e3a554973b8b5442cccba33e5d545660")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyApiProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyApiProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
